b34d33263a68ed5bd761bfc5be3d8f6036dcc39b
Mukundkhunt <64716112+Mukundkhunt@users.noreply.github.com>
2020-10-12 15:22:47 +0530

    Login API added

--- controller/user.email.send.js
+++ controller/user.email.send.js
@@ -1,7 +1,7 @@
 const UserModel = require('../model/user.model')
 const httpStatus = require('http-status')
 module.exports = (req, res) => {
-    if (!req.query.email) {
+    if (!req.query) {
         res.status(httpStatus.INTERNAL_SERVER_ERROR).send("Missing email is required")
     } else {
         UserModel.find({ email: req.query.email, password: req.query.password }).then(result => res.send(result)).catch(err => console.log(err))
